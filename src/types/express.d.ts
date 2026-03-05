f385028a1820ac04f016a8eeaed758e75c60dd89
oddFella-vibing <106969386+oddFella-vibing@users.noreply.github.com>
2026-03-05 15:39:18 +0700

    last stand

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -6,3 +6,13 @@ declare global {
     }
   }
 }
+declare namespace NodeJS {
+  interface ProcessEnv {
+    NODE_ENV: "development" | "production";
+    PORT?: string;
+    MONGO_URI?: string;
+    JWT_SECRET?: string;
+  }
+}
+
+declare var process: NodeJS.Process;
